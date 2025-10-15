ac3d11738c3637c7accb609bac08c5870b11c983
thomas <thomas@thomas-spinergie.(none)>
2025-10-15 10:21:02 +0200

    simpler template

--- frontend/src/components/GamesList.tsx
+++ frontend/src/components/GamesList.tsx
@@ -1,12 +1,47 @@
 import FirestoreData from "./FirestoreData";
 import LogoutButton from "./LogoutButton";
 
+// Sample game data (replace with data from FirestoreData)
+const games = [
+  {
+    id: 1,
+    image:
+      "https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg",
+    alt: "Wingspan",
+    rank: "#38",
+    name: "Wingspan",
+  },
+  {
+    id: 2,
+    image:
+      "https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg",
+    alt: "Cascadia",
+    rank: "#54",
+    name: "Cascadia",
+  },
+];
+
+// Optional: Create a GameCard component for reusability
+function GameCard({ game }) {
+  const { image, alt, rank, name } = game;
+  return (
+    <div className="game-card">
+      <img src={image} alt={alt} />
+      <div className="rank-tag">{rank}</div>
+      <div className="game-name">{name}</div>
+    </div>
+  );
+}
+
 function GamesList() {
+  // If FirestoreData provides the games array, you can replace the static `games` with it
+  // e.g., const games = useContext(FirestoreContext) or passed as props from FirestoreData
+
   return (
     <>
       <header>
         <div className="menu-icon material-icons">menu</div>
-        <h1>Game Winner </h1>
+        <h1>Game Winner</h1>
         <LogoutButton />
       </header>
 
@@ -16,122 +51,14 @@ function GamesList() {
         <div className="tab">History</div>
       </div>
 
-      <section className="section-title">Games (2)</section>
+      <section className="section-title">Games ({games.length})</section>
       <FirestoreData />
+
+      {/* Single game-grid with map */}
       <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
-      </div>
-
-      <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
-      </div>
-      <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
-      </div>
-      <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
-      </div>
-      <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
-      </div>
-      <div className="game-grid">
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/yLZJCVLlIx4c7eJEWUNJ7w__itemrep/img/DR7181wU4sHT6gn6Q1XccpPxNHg=/fit-in/246x300/filters:strip_icc()/pic4458123.jpg"
-            alt="Wingspan"
-          />
-          <div className="rank-tag">#38</div>
-          <div className="game-name">Wingspan</div>
-        </div>
-
-        <div className="game-card">
-          <img
-            src="https://cf.geekdo-images.com/MjeJZfulbsM1DSV3DrGJYA__imagepage/img/0ksox22FKLq-Z-rsbBlF2IDG9x0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic5100691.jpg"
-            alt="Cascadia"
-          />
-          <div className="rank-tag">#54</div>
-          <div className="game-name">Cascadia</div>
-        </div>
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
       </div>
 
       <div className="fab material-icons">add</div>
